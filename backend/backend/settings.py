d669e5497e88173c28bed2c690499e5a766de7ee
Rashid-24u <rashidridhu10@gmail.com>
2026-03-14 11:42:58 +0530

    connected frontend to render backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = 'django-insecure-cs1r4aw+=e#r!ghtl3@1e+i)_&2u6o*#!n5!89pww!(ya%8uqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["shan-associates.onrender.com"]
+ALLOWED_HOSTS = [
+    "shan-associates.onrender.com",
+    "127.0.0.1",
+    "localhost"
+]
 
 
 # Application definition
